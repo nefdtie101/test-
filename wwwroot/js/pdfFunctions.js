d36f76255a738235d9cbb657889c1d58b8d5ccad
johann <johann@nefdtco.co.za>
2023-12-03 21:50:44 +0200

    fixed error

--- wwwroot/js/pdfFunctions.js
+++ wwwroot/js/pdfFunctions.js
@@ -2,5 +2,5 @@
     var element = document.getElementById(divId);
 
     // Send the HTML content to the server for PDF conversion
-    DotNet.invokeMethod('YourProjectNamespace', 'ConvertHtmlToPdf', element.innerHTML, fileName);
+    DotNet.invokeMethodAsync('YourProjectNamespace', 'ConvertHtmlToPdf', element.innerHTML, fileName);
 }
